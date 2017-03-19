3cabc79ca622065253e3039f4d2b4a53c1c42595
trung <thanhtrungnguyen1208@gmail.com>
2017-03-19 08:48:34 +0700

    Ttung

--- src/DAO/MySQLHumanDAO.java
+++ src/DAO/MySQLHumanDAO.java
@@ -1,7 +1,11 @@
 package DAO;
 
+import Model.Book;
 import Model.Human;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class MySQLHumanDAO implements HumanDAO {
 
@@ -13,22 +17,75 @@ public class MySQLHumanDAO implements HumanDAO {
 
     @Override
     public Connection getConnection() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return getConnection("kttk", "root", "");
     }
 
     @Override
     public Connection getConnection(String dbName, String username, String password) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String dbUrl = "jdbc:mysql://localhost:3306/" + dbName;
+        String dbClass = "com.mysql.jdbc.Driver";
+        try {
+            Class.forName(dbClass);
+            conn = DriverManager.getConnection(dbUrl,
+                    username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
     }
 
     @Override
     public boolean login(String username, String password) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT * FROM account WHERE Username = ? AND Password = ?";
+        PreparedStatement ps = null;
+        ResultSet res = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            res = ps.executeQuery();
+            while (res.next()) {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            DBUtil.closeResultSet(res);
+        }
+        return false;
     }
 
     @Override
     public Human getInfo(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String sql = "SELECT * FROM human WHERE id = ?";
+        PreparedStatement ps = null;
+        ResultSet res = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+
+            res = ps.executeQuery();
+            while (res.next()) {
+                Human human = new Human() {
+
+                    @Override
+                    public String getDiscriminator() {
+                        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                    }
+                };
+                
+
+                return human;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            DBUtil.closeResultSet(res);
+        }
+        return null;
     }
 
 }
--- src/DAO/SQLServerHumanDAO.java
+++ src/DAO/SQLServerHumanDAO.java
@@ -2,8 +2,14 @@ package DAO;
 
 import Model.Human;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class SQLServerHumanDAO implements HumanDAO {
+    
+    String hostName = "localhost";
+    String sqlInstanceName = "SQLEXPRESS";
 
     private Connection conn;
 
@@ -18,12 +24,39 @@ public class SQLServerHumanDAO implements HumanDAO {
 
     @Override
     public Connection getConnection(String dbName, String username, String password) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         String dbUrl = "jdbc:sqlserver://" + hostName + ":1433;instance=" + sqlInstanceName + ";databaseName=" + dbName;
+        String dbClass = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+        try {
+            Class.forName(dbClass);
+            conn = DriverManager.getConnection(dbUrl,
+                    username, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
     }
 
     @Override
     public boolean login(String username, String password) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       String sql = "SELECT * FROM account WHERE Username = ? AND Password = ?";
+        PreparedStatement ps = null;
+        ResultSet res = null;
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            res = ps.executeQuery();
+            while (res.next()) {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DBUtil.closePreparedStatement(ps);
+            DBUtil.closeResultSet(res);
+        }
+        return false;
     }
 
     @Override
